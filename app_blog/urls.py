52832388764cd0d53c4319f3def5f17c54bb275c
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-12 17:24:56 +0545

    third commit

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('post/create/', views.CreatePostView.as_view(), name="create"),
     path('post/<int:pk>/update/', views.UpdatePostView.as_view(), name="update"),
     path('post/<int:pk>/delete/', views.DeletePostView.as_view(), name="delete"),
-    path('post/draft', views.DraftListView.as_view(), name="draft")
+    path('post/draft', views.DraftListView.as_view(), name="draft"),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment'),
+    path('post/<int:pk>/arpove', views.comment_approve, name='comment_approve'),
+    path('post/<int:pk>/delete', views.commnet_remove, name='comment_remove'),
+    path('post/<int:pk>/publish', views.post_pusblish, name='post_publish')
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Comment, Post
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .forms import PostForm, CommentFrom
-from django.views.generic import (CreateView, 
-                                    DeleteView, 
-                                    DetailView, 
-                                    FormView, 
-                                    UpdateView,
-                                    TemplateView,
-                                    ListView)
+from django.views.generic import (CreateView, DeleteView, 
+                                    DetailView, UpdateView,
+                                    TemplateView, ListView)
 
 # Create your views here
 
@@ -71,3 +67,47 @@ class DraftListView(ListView, login_required):
 
     def get_queryset(self):
         return  Post.objects.filter(published_date__isnull = True).order_by('creadted_date')
+
+
+###########################################################################################
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentFrom(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', pk = post.pk)
+
+        else:
+            form = CommentFrom()
+
+        return render(request, 'comment_form.html', {'form':form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+
+    return redirect('post_detail',pk=comment.post.pk)
+
+
+@login_required
+def commnet_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail.html', pk=post_pk)
+
+
+@login_required
+def post_pusblish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
